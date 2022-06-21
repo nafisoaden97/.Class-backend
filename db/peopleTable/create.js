7b94900a31ede005265602d72e4199a75558f71e
IsmailAli <ismailali98@gmail.com>
2022-06-21 10:33:24 +0100

    Populating table functions complete

---
+++ db/peopleTable/create.js
@@ -0,0 +1,9 @@
+import {query} from "../index.js";
+
+const sqlstring = `CREATE TABLE IF NOT EXISTS profiles (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name TEXT, region TEXT, jobTitle TEXT, pronouns TEXT, imageLink TEXT, contactInfo TEXT);`;
+
+async function createProfilesTable(){
+   const res = await query (sqlstring);
+   console.log(res.command, "table created");
+}
+createProfilesTable();
---
+++ libs/data.js
@@ -0,0 +1,40 @@
+const profile = [
+    {
+        id: 1,
+        name: "Sam Wylie",
+        region: "Southwest",
+        jobTitle: "Bootcamper",
+        pronouns: "he/him",
+        imageLink: "placeholder",
+        contactInfo: "placeholder"
+    },
+    {
+        id: 2,
+        name: "Ismail Ali",
+        region: "West Midlands",
+        jobTitle: "Bootcamper",
+        pronouns: "he/him",
+        imageLink: "placeholder",
+        contactInfo: "placeholder"
+    },
+    {
+        id: 3,
+        name: "Nafiso Aden",
+        region: "West Midlands",
+        jobTitle: "Bootcamper",
+        pronouns: "----",
+        imageLink: "placeholder",
+        contactInfo: "placeholder"
+    },
+    {
+        id: 4,
+        name: "Madison Lowe",
+        region: "Yorkshire",
+        jobTitle: "Bootcamper",
+        pronouns: "----",
+        imageLink: "placeholder",
+        contactInfo: "placeholder"
+    }
+]
+
+export default profile; 
