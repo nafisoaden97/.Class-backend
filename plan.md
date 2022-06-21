9c5f1ff4f8b44c5d5c718b32cb1105a46e27382b
IsmailAli <ismailali98@gmail.com>
2022-06-21 12:05:53 +0100

    get by name/region function not working

--- plan.md
+++ plan.md
@@ -2,13 +2,17 @@ Back-end plan Tuesday
 
 # Mock database in libs folder - an object containing all the categories we want right now --DONE--
 
- - Id, Name, Region, Job Title, Pronouns, Image Link, Contact Info, Links
+ - Id, Bootcamper, Name, Region, Job Title, Pronouns, Image Link, Contact Info, Links
 
 # Do our database intiation SQL things like create and populate, using the data from our libs to test --DONE--
 
 # Import the environment variables into db/index.js and link up with our data?
 
 # Route functions - start with get all for now, and only after, migrate this to the models section
+    - created a get function to get all profiles
+    - create a get function that gets by region
+    - get function that gtes by bootcamp number
+    - gets by searched name
 
 # Look into encryption and information safety to incorporate
 
--- routes/router.js
+++ routes/router.js
@@ -8,9 +8,36 @@ async function getProfiles() {
     return response.rows;
 }
 
+
 router.get("/", async function (req, res) {
     let response = await getProfiles();
     res.json({success: true, payload: response});
 });
+
+/* async function getProfilesByName(name) {
+    const response = await pool.query(`SELECT * FROM profiles WHERE LOWER(name)= LOWER(${name});`);
+    return response.rows;
+}
+
+router.get("/", function (req, res) {
+    let searchTerm = req.body.name.toLowerCase();
+    console.log(req.body);
+    payload = getProfilesByName(searchTerm)
+    res.json({ success: true, payload: payload });
+});
+
+export default router; */
+
+router.get("/", function (req, res) {
+    let searchTerm = req.query.name.toLowerCase();
+    console.log(req.query.region);
+    payload = getProfilesByRegion(searchTerm)
+    res.json({ success: true, payload: payload });
+});
+async function getProfilesByRegion(region) {
+    const response = await pool.query(`SELECT * FROM profiles WHERE LOWER(region)= LOWER(${region});`);
+    return response.rows;
+}
+
 
 export default router;
