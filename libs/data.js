617c9f0239cda7c3d1b1bb7d87bfa93b731ebfeb
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-06-22 12:02:06 +0100

    10 profiles added to the data.js

--- libs/data.js
+++ libs/data.js
@@ -3,11 +3,12 @@ export const profile = [
         id: 1,
         bootcampNumber: 12,
         name: "Sam Wylie",
-        region: "Southwest",
+        region: "South west",
         jobTitle: "Bootcamper",
+        desc: "Putting together a hackathon team, see full bio for more details!",
         pronouns: "he/him",
-        imageLink: "placeholder",
-        contactInfo: "placeholder"
+        imageLink:"https://cdn.discordapp.com/attachments/988860447098810369/988860501704470618/freepik3b.jpg",
+        contactInfo: "samwylie@gmail.com"
     },
     {
         id: 2,
@@ -16,8 +17,9 @@ export const profile = [
         region: "West Midlands",
         jobTitle: "Bootcamper",
         pronouns: "he/him",
-        imageLink: "placeholder",
-        contactInfo: "placeholder"
+        desc :"Putting together a hackathon team, see full bio for more details!",
+        imageLink: "https://cdn.discordapp.com/attachments/988860447098810369/988860502195179520/freepik3d.jpg",
+        contactInfo: "ismailali@gmail.com"
     },
     {
         id: 3,
@@ -25,9 +27,10 @@ export const profile = [
         name: "Nafiso Aden",
         region: "West Midlands",
         jobTitle: "Bootcamper",
+        desc: "Available for coffee chats! Slow with email, quicker via PM.",
         pronouns: "she/her",
-        imageLink: "placeholder",
-        contactInfo: "placeholder"
+        imageLink: "https://cdn.discordapp.com/attachments/657862753687699467/988860108509425694/freepik3a.jpg",
+        contactInfo: "nafisoaden@gamil.com"
     },
     {
         id: 4,
@@ -35,10 +38,76 @@ export const profile = [
         name: "Madison Lowe",
         region: "Yorkshire",
         jobTitle: "Bootcamper",
-        pronouns: "placeholder",
-        imageLink: "placeholder",
-        contactInfo: "placeholder"
-    }
+        pronouns: "she/her",
+        desc: "Open to work! See page for GitHub and portfolio.",
+        imageLink: "https://cdn.discordapp.com/attachments/988860447098810369/988860503348617357/freepik4c.jpg",
+        contactInfo: "madisonLowe@outlook.co.uk"
+    },{
+        id: 5,
+        bootcampNumber: 10,
+        name: "jim Nasium",
+        region: "North west",
+        jobTitle: "Back-End DevOps",
+        desc: "Roses are red, violets are blue, unexpected '{' on line 32.",
+        pronouns: "he/him",
+        imageLink: "https://cdn.discordapp.com/attachments/988860447098810369/988860501704470618/freepik3b.jpg",
+        contactInfo: "jim84@hotmail.com"
+
+    },
+    {   id: 6,
+        bootcampNumber: 9,
+        name: "Emma Nate",
+        region: "North East",
+        jobTitle: "UX and UI",
+        pronouns: "she/her",
+        desc: "Available for coffee chats! Slow with email, quicker via PM.",
+        imageLink:"https://cdn.discordapp.com/attachments/988860447098810369/988860501956100106/freepik3c.jpg",
+        contactInfo: "emmanate@outlook.com"
+
+    },
+    {   id: 7,
+        bootcampNumber: 3,
+        name: "liz Koufman",
+        region: "East Midland",
+        jobTitle: "Coach at School of Code",
+        pronouns: "she/her",
+        desc: "Available for Tea chats! Slow with email, quicker via PM.",
+        imageLink:"https://cdn.discordapp.com/attachments/988860447098810369/988860503348617357/freepik4c.jpg",
+        contactInfo: "lizk@outlook.com"
+
+    },
+    {   id: 8,
+        bootcampNumber: 1,
+        name: "Chris Meah",
+        region: "West Midland",
+        jobTitle: "CEO of School of Code",
+        pronouns: "he/him",
+        desc: "I'd rate my programming puns C++.",
+        imageLink:"https://cdn.discordapp.com/attachments/988860447098810369/988860503071784970/freepik4b.jpg",
+        contactInfo: "chris-meah@hotmail.co.uk"
+    },
+    {   id: 9,
+        bootcampNumber: 5,
+        name: " chole young",
+        region: "Greater London",
+        jobTitle: "Cyber Security ",
+        pronouns: "her/she",
+        desc: "Taking sign-ups for a graduates D&D game! Get in touch for details!",
+        imageLink:"https://cdn.discordapp.com/attachments/988860447098810369/988860502765617202/freepik4a.jpg",
+        contactInfo: "chloe-young1@hotmail.co.uk"
+    },
+
+{     id: 10,
+        bootcampNumber: 6,
+        name: "Sara Nader",
+        region: "South West",
+        jobTitle: "Front-End Developer ",
+        pronouns: "her/she",
+        desc: "Why do Java programmers have to wear glasses? They don't C#.",
+        imageLink: "https://cdn.discordapp.com/attachments/988860447098810369/988860503935827978/freepik4e.jpg",
+        contactInfo: "sara-nader@hotmail.co.uk"
+    },
+
 ]
 
 export default profile;
