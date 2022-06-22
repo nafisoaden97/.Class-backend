6429a51baaaada56738a89f5da14918b689da83b
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-06-22 10:48:58 +0100

    new port created

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import router from "./routes/router.js";
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 9000;
 
 app.get("/", function (req, res) {
     res.json({
