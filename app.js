811921b40b6c3ba8415ec72c5a24f2665b418bf5
nafisoaden97 <nafisoaden97@gmail.com>
2022-08-31 11:37:48 +0100

    port changed, heroku deployed

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 import express from "express";
 import router from "./routes/router.js";
- import cors from "cors";
+import cors from "cors";
 const app = express();
-const PORT = process.env.port || 9000;
-
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
 app.get("/", function (req, res) {
-    res.json({
-      success: true,
-      message: "Test working",
-    });
+  res.json({
+    success: true,
+    message: "Test working",
   });
-  app.use(cors());
-  app.use(express.json());
-  app.use("/profiles", router);
-  
-  app.listen(PORT, function () {
-    console.log(`Server is running on port ${PORT}`);
-  });
+});
+app.use(cors());
+app.use(express.json());
+app.use("/profiles", router);
 
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
