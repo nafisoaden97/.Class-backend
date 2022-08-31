bb0edbf1f3e74891fb7408be888cc61d64850065
nafisoaden97 <nafisoaden97@gmail.com>
2022-08-31 14:48:51 +0100

    y

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import router from "./routes/router.js";
-import cors from "cors";
+// import cors from "cors";
 const app = express();
 const PORT = process.env.PGPORT || 9000;
 
